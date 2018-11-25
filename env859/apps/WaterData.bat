b23f19376c51e2b5d2f8d1a3b617888105514cef
John Fay <john.fay@duke.edu>
2018-11-25 17:07:38 -0500

    Renames to WaterData.*

---
+++ env859/apps/WaterData.bat
@@ -0,0 +1,2 @@
+%localappdata%\ESRI\conda\envs\my_vis\python.exe -m bokeh serve WaterData.py --show
+pause
--- env859/apps/app.bat
+++
@@ -1,4 +0,0 @@
-ECHO Hit ctrl-c to exit the app
-explorer "http://localhost:5006/app
-%localappdata%\ESRI\conda\envs\my_vis\python.exe -m bokeh serve app.py
-pause
